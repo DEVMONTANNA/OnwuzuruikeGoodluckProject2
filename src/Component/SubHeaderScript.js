612e111a5793e634d85028c94132ce290d33365e
DEVMONTANNA <chiedoziegoodluck351@gmail.com>
2026-08-27 01:57:32 +0100

    Fix: Remove broken Learning import from Home.js

--- src/Component/SubHeaderScript.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SubHeaderScript = () => {
-  return <div>SubHeaderScript</div>;
-};
-
-export default SubHeaderScript;
--- src/Pages/Homescript.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Homescript = () => {
-  return <div>Homescript</div>;
-};
-
-export default Homescript;
